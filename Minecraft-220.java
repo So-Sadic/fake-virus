e441cd27ecacd7cc4a160ce1fc894367fce29b66
So-Sadic <65101584+So-Sadic@users.noreply.github.com>
2020-05-15 20:41:45 -0300

    Update Minecraft-220.java

--- Minecraft-220.java
+++ Minecraft-220.java
@@ -1,16 +1,28 @@
 import javax.swing.JOptionPane;
+import javax.swing.*;
+import javax.swing.JFrame;
+import java.awt.event.*;
+import pop.java;
 
-class juego
-{
+package JApplet;
+ 
     public static void main(String[] args)
     {
-        System.out.println("There's is an virus");//Opcion 1
+        System.out.println("There's an virus");//Opcion 1
+int game2=0;
+int game= 0;
+int game3=0;
+int gamef=0;
+int so= 0;
 
-       int juego= JOptionPane.showConfirmDialog(null,"There's is an virus","Windows defender xd", JOptionPane.YES_NO_CANCEL_OPTION);//Opcion 2
+       int juego= JOptionPane.showConfirmDialog(null,"There's an virus","Windows defender", JOptionPane.YES_NO_CANCEL_OPTION);//Opcion 2
 
-           
-    }
+          if (juego == JOptionPane.YES_OPTION){
+        game2 = JOptionPane.showOptionDialog(null, "Your computer will get corrupted due to the virus caused by bill gates", "Windows defender", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
 }
 
 
-// execute this from the terminal 
+if (game2== JOptionPane.YES_OPTION){
+  game = JOptionPane.showOptionDialog(null, "I lied", "An lie.", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
+}
+}
