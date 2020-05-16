7bb541398ce5216de274640db7717f586a497e00
So-Sadic <65101584+So-Sadic@users.noreply.github.com>
2020-05-15 21:01:56 -0300

    Update Minecraft-220.java

--- Minecraft-220.java
+++ Minecraft-220.java
@@ -18,11 +18,10 @@ int so= 0;
        int juego= JOptionPane.showConfirmDialog(null,"There's an virus","Windows defender", JOptionPane.YES_NO_CANCEL_OPTION);//Opcion 2
 
           if (juego == JOptionPane.YES_OPTION){
-        game2 = JOptionPane.showOptionDialog(null, "Your computer will get corrupted due to the virus caused by bill gates", "Windows defender", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-}
+        game2 = JOptionPane.showOptionDialog(null, "Your computer will get corrupted due to the virus caused by bill gates", "Windows defender", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);}
 
 
 if (game2== JOptionPane.YES_OPTION){
-  game = JOptionPane.showOptionDialog(null, "I lied", "An lie.", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);
-}
+  game = JOptionPane.showOptionDialog(null, "I lied", "An lie.", JOptionPane.OK_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, null, null, null);}
+
 }
